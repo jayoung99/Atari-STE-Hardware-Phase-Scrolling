343ac402c63c9400f5b63e6e7c2d3c3b43b01d0b
jayoung99 <47805244+jayoung99@users.noreply.github.com>
2026-02-03 19:59:22 -0500

    set resolution to low specifically

--- assemble.sh
+++ assemble.sh
@@ -3,11 +3,8 @@
 #Phase Scroll Demo
 name="Phase Scroll"
 target=PHSCROLL
-debug=PHSCRL_D
+#debug=PHSCRL_D
 src=PHSCROLL.S
-if [ -e "$target".BIN ];then
-	rm "$target".BIN
-fi	
 
 #assemble and link debug version w/external symbol table
 #vasm -m68000 -Felf -devpac -o "$target".O "$src"
